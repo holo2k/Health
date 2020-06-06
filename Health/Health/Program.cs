b6c7e33ea6c28e0cb0d04dadbc82412b7f77f052
iamHolo <nneketaa@yandex.ru>
2020-06-06 20:25:40 +0500

    ye

---
+++ Health/Health/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Health
+{
+    class Health
+    {
+        private int temperature;
+        public int Temperature
+        {
+            get
+            {
+                return temperature;
+            }
+            set
+            {
+                if (value < 34)
+                    temperature = 34;
+                if (value > 41)
+                    temperature = 41;
+                else temperature = value;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Health Health = new Health();
+            Health.Temperature = 55;
+            Console.WriteLine(Health.Temperature + " (В коде задано число > 41)");
+            Health.Temperature = 36;
+            Console.WriteLine(Health.Temperature);
+        }
+    }
+}
